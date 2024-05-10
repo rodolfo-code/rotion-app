df457ff775fe820a4d8cf7925bc352525b230b7c
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2024-05-10 00:17:12 -0300

    criando modal de ediçao deleçao de card

--- app/workspace/components/CardMenu.tsx
+++ app/workspace/components/CardMenu.tsx
@@ -22,43 +22,83 @@ import { CiEdit } from "react-icons/ci";
 
 import { MdOutlineAutoFixHigh, MdOutlineDelete } from "react-icons/md";
 
-export function CardMenu() {
+import { CalendarIcon, EnvelopeClosedIcon, FaceIcon, GearIcon, PersonIcon, RocketIcon, TrashIcon, Pencil2Icon } from "@radix-ui/react-icons";
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { useEffect, useRef, useState } from "react";
+
+export function CardMenu({ onClose }: any) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  // Ref para o elemento do modal
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // Função para fechar o modal
+  const closeModal = () => {
+    setIsOpen(false);
+    onClose();
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        closeModal();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <Menubar className=" h-5 p-2 w-8 hover:bg-neutral-100">
-      <MenubarMenu>
-        <MenubarTrigger className="h-5 p-0">
-          <RxDotsHorizontal className="self-center" />
-        </MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem className="h-5 text-sm">
-            Edit
-            <MenubarShortcut>
-              <MdOutlineAutoFixHigh size={16} />
-            </MenubarShortcut>
-          </MenubarItem>
-          <MenubarSeparator />
-          <MenubarItem className="h-5 text-xs">
-            Delete
-            <MenubarShortcut>
-              <MdOutlineDelete size={16} />
-            </MenubarShortcut>
-          </MenubarItem>
-          {/* <MenubarItem disabled>New Incognito Window</MenubarItem>
-          <MenubarSeparator /> */}
-          {/* <MenubarSub>
-            <MenubarSubTrigger>Share</MenubarSubTrigger>
-            <MenubarSubContent>
-              <MenubarItem>Email link</MenubarItem>
-              <MenubarItem>Messages</MenubarItem>
-              <MenubarItem>Notes</MenubarItem>
-            </MenubarSubContent>
-          </MenubarSub> */}
-          {/* <MenubarSeparator />
-          <MenubarItem>
-            Print... <MenubarShortcut>⌘P</MenubarShortcut>
-          </MenubarItem> */}
-        </MenubarContent>
-      </MenubarMenu>
-    </Menubar>
+    <Command className="rounded-lg border shadow-md z-50 " ref={modalRef}>
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem className="">
+            <Pencil2Icon className="mr-2 h-4 w-4" />
+            <span>Edit</span>
+          </CommandItem>
+          <CommandItem>
+            <TrashIcon className="mr-2 h-4 w-4" />
+            <span>Delete</span>
+          </CommandItem>
+          <CommandItem>
+            <RocketIcon className="mr-2 h-4 w-4" />
+            <span>Launch</span>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <PersonIcon className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <EnvelopeClosedIcon className="mr-2 h-4 w-4" />
+            <span>Mail</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <GearIcon className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
   );
 }
--- app/workspace/components/TaskCard.tsx
+++ app/workspace/components/TaskCard.tsx
@@ -3,17 +3,56 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 
 import { BiRadioCircle } from "react-icons/bi";
+import { cn } from "@/lib/utils";
+import { ArrowUpCircle, CheckCircle2, Circle, HelpCircle, LucideIcon, MoreHorizontal, XCircle } from "lucide-react";
 
 import { Card } from "@/app/types";
 import { Sheet } from "@/components/Sheet";
 import { Textarea } from "@/components/ui/textarea";
 import { CardMenu } from "./CardMenu";
 
+import { MdOutlineAutoFixHigh, MdOutlineDelete } from "react-icons/md";
+import { RxDotsHorizontal } from "react-icons/rx";
+
 type TaskCardTypes = {
   data: Card;
   color: string | undefined;
   handleSetNewTask: (event: ChangeEvent<HTMLTextAreaElement>) => void;
 };
+
+type Status = {
+  value: string;
+  label: string;
+  icon: LucideIcon;
+};
+
+const statuses: Status[] = [
+  {
+    value: "backlog",
+    label: "Backlog",
+    icon: HelpCircle,
+  },
+  {
+    value: "todo",
+    label: "Todo",
+    icon: Circle,
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+    icon: ArrowUpCircle,
+  },
+  {
+    value: "done",
+    label: "Done",
+    icon: CheckCircle2,
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+    icon: XCircle,
+  },
+];
 
 export default function TaskCard(props: TaskCardTypes) {
   const {
@@ -22,6 +61,9 @@ export default function TaskCard(props: TaskCardTypes) {
     handleSetNewTask,
   } = props;
   const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState<Status | null>(null);
+  const [showModal, setShowModal] = useState(false);
 
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -50,15 +92,19 @@ export default function TaskCard(props: TaskCardTypes) {
   return (
     <div className="group relative">
       <div className="hidden z-50 left-[87%] top-[6%] group-hover:block absolute">
-        {/* <CardMenu /> */}
-        OLAAAAA
+        <div className="flex h-4 p-2 w-8 hover:bg-neutral-100 relative ">
+          <RxDotsHorizontal className="self-center" onClick={() => setOpen(true)} />
+        </div>
       </div>
+      <div className="absolute z-50 left-64 top-3 w-56">{open && <CardMenu onClose={() => setOpen(false)} />}</div>
+      {/* <div className="relative z-50"></div> */}
+
       <Sheet.Root>
         <Sheet.Trigger>
           <div className="hover:bg-gray-50 transition-colors hover:shadow-sm shadow-sm bg-white rounded min-h-12 relative cursor-pointer border-[1px]">
-            <div id="header" className="flex justify-end p-0.5">
+            {/* <div id="header" className="flex justify-end p-0.5">
               <BiRadioCircle color={color} />
-            </div>
+            </div> */}
             <div className="flex flex-wrap px-3 pb-3">
               <p className="text-wrap text-xs text-gray-900 font-medium dark:text-gray-400 mt-2">{content}</p>
             </div>
